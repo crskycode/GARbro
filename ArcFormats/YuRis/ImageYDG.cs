bf8e14613950e5fe77e978852a7f0586f6eed706
Crsky <crskycode@hotmail.com>
2026-04-12 18:58:42 +0800

    Extract QoiCodec

--- ArcFormats/YuRis/ImageYDG.cs
+++ ArcFormats/YuRis/ImageYDG.cs
@@ -30,6 +30,7 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Media;
+using GameRes.Formats.QoiCodec;
 using GameRes.Utility;
 
 namespace GameRes.Formats.YuRis
@@ -231,97 +232,6 @@ namespace GameRes.Formats.YuRis
                     dst += 4;
                 }
                 return output;
-            }
-        }
-
-        static class QoiCodec
-        {
-            public const int Index = 0x00;
-            public const int Diff = 0x40;
-            public const int Luma = 0x80;
-            public const int Run = 0xC0;
-            public const int Rgb = 0xFE;
-            public const int Rgba = 0xFF;
-            public const int Mask2 = 0xC0;
-            public const int HashTableSize = 64;
-        }
-
-        class QoiDecodeStream
-        {
-            readonly IBinaryStream m_input;
-            readonly byte[] m_table;
-            uint m_pixel;
-
-            public QoiDecodeStream(IBinaryStream input)
-            {
-                m_input = input;
-                m_table = new byte[4 * QoiCodec.HashTableSize];
-                m_pixel = 0xFF000000;
-            }
-
-            public int Read(out uint output)
-            {
-                var r = (byte)m_pixel;
-                var g = (byte)(m_pixel >> 8);
-                var b = (byte)(m_pixel >> 16);
-                var a = (byte)(m_pixel >> 24);
-                var run = 1;
-                var b1 = m_input.ReadByte();
-                if (-1 == b1)
-                    throw new EndOfStreamException();
-
-                if (QoiCodec.Rgb == b1)
-                {
-                    var rgb = m_input.ReadInt24();
-                    r = (byte)rgb;
-                    g = (byte)(rgb >> 8);
-                    b = (byte)(rgb >> 16);
-                }
-                else if (QoiCodec.Rgba == b1)
-                {
-                    var rgba = m_input.ReadInt32();
-                    r = (byte)rgba;
-                    g = (byte)(rgba >> 8);
-                    b = (byte)(rgba >> 16);
-                    a = (byte)(rgba >> 24);
-                }
-                else if (QoiCodec.Index == (b1 & QoiCodec.Mask2))
-                {
-                    var p1 = (b1 & ~QoiCodec.Mask2) * 4;
-                    r = m_table[p1];
-                    g = m_table[p1 + 1];
-                    b = m_table[p1 + 2];
-                    a = m_table[p1 + 3];
-                }
-                else if (QoiCodec.Diff == (b1 & QoiCodec.Mask2))
-                {
-                    r += (byte)(((b1 >> 4) & 0x03) - 2);
-                    g += (byte)(((b1 >> 2) & 0x03) - 2);
-                    b += (byte)((b1 & 0x03) - 2);
-                }
-                else if (QoiCodec.Luma == (b1 & QoiCodec.Mask2))
-                {
-                    var b2 = m_input.ReadByte();
-                    if (-1 == b2)
-                        throw new EndOfStreamException();
-                    var vg = (b1 & 0x3F) - 32;
-                    r += (byte)(vg - 8 + ((b2 >> 4) & 0x0F));
-                    g += (byte)vg;
-                    b += (byte)(vg - 8 + (b2 & 0x0F));
-                }
-                else if (QoiCodec.Run == (b1 & QoiCodec.Mask2))
-                {
-                    run = (b1 & 0x3F) + 1;
-                }
-
-                var p2 = (r * 3 + g * 5 + b * 7 + a * 11) % QoiCodec.HashTableSize * 4;
-                m_table[p2    ] = r;
-                m_table[p2 + 1] = g;
-                m_table[p2 + 2] = b;
-                m_table[p2 + 3] = a;
-                m_pixel = (uint)(r | (g << 8) | (b << 16) | (a << 24));
-                output = (uint)(b | (g << 8) | (r << 16) | (a << 24));
-                return run;
             }
         }
     }
